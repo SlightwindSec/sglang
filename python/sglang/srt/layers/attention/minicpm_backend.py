[Fix] Fix softmax bug (#12)

python/sglang/srt/layers/attention/minicpm_backend.py
@@ -579,15 +579,17 @@ class MiniCPMSparseBackend(AttentionBackend):
             if all_sparse:
                 # all batch is sparse
                 metadata = self.forward_metadata
-                compressed_k = torch.zeros(
+                compressed_k = torch.full(
                     (forward_batch.batch_size * self.max_context_len // self.k1_kernel_stride, self.head_group_num, self.head_dim),
                     dtype=torch.bfloat16,
-                    device=self.device
-                        )
-                compressed_k2 = torch.zeros(
+                    device=self.device,
+                    fill_value=float('-inf')
+                )
+                compressed_k2 = torch.full(
                     (forward_batch.batch_size * self.max_context_len // self.k2_kernel_stride, self.head_group_num, self.head_dim), 
                     dtype=torch.bfloat16, 
-                    device=self.device
+                    device=self.device,
+                    fill_value=float('-inf')
                 )
 
                 get_compress_k_v2(

python/sglang/srt/layers/attention/minicpm_sparse_utils.py
@@ -503,8 +503,9 @@ def compressed_attention(
             )
     
             scale = 1.0 / math.sqrt(head_dim)
-            score = torch.bmm(q_reshape, k_reshape) * scale
-            score = F.softmax(score, dim=-1)
+            score = torch.bmm(q_reshape, k_reshape).mul_(scale)
+            torch.nan_to_num(score, nan=float("-inf"), posinf=float("-inf"), out=score)
+            torch.softmax(score, dim=-1, out=score)
             score = score.reshape(kv_head, batch_size, group_size, k1_len // batch_size).sum(dim=2)
         else:  
             score = infllmv2_attn_stage1(